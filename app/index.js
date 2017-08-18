f7847189e185c7b26b62bf9164eece9d9fd74d17
SarahhhC <joytutu29@gmail.com>
2017-08-18 14:59:16 +0900

    make app folder

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
